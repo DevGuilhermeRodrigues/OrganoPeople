c8d87b1f2119ad95169f38696f5c4715161e5775
DevGuilhermeRodrigues <guilhermegrillo25@gmail.com>
2024-07-19 22:43:08 -0300

    Adicionado o banner

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import imgBanner from "../../assets/imagens/banner.png";
+
+function Banner() {
+    return (
+        <>
+          <img src={imgBanner} alt="" />
+        </>
+    )
+}
+
+export default Banner;
